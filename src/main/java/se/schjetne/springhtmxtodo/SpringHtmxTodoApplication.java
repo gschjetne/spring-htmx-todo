cb589de34284f3f83e642ef42279a2a34817d5b0
Grim Schjetne <grim@schjetne.se>
2022-04-23 09:54:29 +0200

    Create project boilerplate

---
+++ src/main/java/se/schjetne/springhtmxtodo/SpringHtmxTodoApplication.java
@@ -0,0 +1,13 @@
+package se.schjetne.springhtmxtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHtmxTodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHtmxTodoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/schjetne/springhtmxtodo/SpringHtmxTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package se.schjetne.springhtmxtodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHtmxTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
